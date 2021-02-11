58fd1f3da9ab26d677ce42ef82614d78f47decfa
ALIANS-PRODUCTIONS <ansar_ali_esq@hotmail.com>
2021-02-11 00:30:50 +0000

    modified for Python 3 and tf 2.5

--- agent/agent.py
+++ agent/agent.py
@@ -1,8 +1,28 @@
-import keras
-from keras.models import Sequential
-from keras.models import load_model
-from keras.layers import Dense
-from keras.optimizers import Adam
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, BatchNormalization 
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import mixed_precision
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+	try:
+		# Currently, memory growth needs to be the same across GPUs
+		for gpu in gpus:
+			tf.config.experimental.set_memory_growth(gpu, True)
+		logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+		print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+	except RuntimeError as e:
+		# Memory growth must be set before GPUs have been initialized
+		print(e)
+
+policy=mixed_precision.Policy('mixed_float16')
+mixed_precision.set_global_policy(policy)
+
+#from tf.keras.models import Sequential
+#from tf.keras.models import load_model
+#from tf.keras.layers import Dense
+#from tf.keras.optimizers import Adam
 
 import numpy as np
 import random
@@ -22,16 +42,22 @@ class Agent:
 		self.epsilon_min = 0.01
 		self.epsilon_decay = 0.995
 
-		self.model = load_model("models/" + model_name) if is_eval else self._model()
+		self.model = tf.keras.models.load_model("models/" + model_name) if is_eval else self._model()
 
 	def _model(self):
-		model = Sequential()
-		model.add(Dense(units=64, input_dim=self.state_size, activation="relu"))
-		model.add(Dense(units=32, activation="relu"))
-		model.add(Dense(units=8, activation="relu"))
-		model.add(Dense(self.action_size, activation="linear"))
-		model.compile(loss="mse", optimizer=Adam(lr=0.001))
+		#model = Sequential()
+		#model.add(Dense(units=64, input_dim=self.state_size, activation="relu"))
+		#model.add(Dense(units=32, activation="relu"))
+		#model.add(Dense(units=8, activation="relu"))
+		#model.add(Dense(self.action_size, activation="linear"))
 
+		model = tf.keras.models.Sequential([
+			tf.keras.layers.Dense(64,activation='relu', input_shape=(self.state_size,)),
+			tf.keras.layers.Dense(32, activation='relu'),
+			tf.keras.layers.Dense(8, activation='relu'),
+			tf.keras.layers.Dense(self.action_size, activation='relu')])
+		#model.compile(loss="mse", optimizer=Adam(lr=0.001))
+		model.compile(optimizer=Adam(lr=0.001), loss='mse')
 		return model
 
 	def act(self, state):
@@ -44,7 +70,7 @@ class Agent:
 	def expReplay(self, batch_size):
 		mini_batch = []
 		l = len(self.memory)
-		for i in xrange(l - batch_size + 1, l):
+		for i in range(l - batch_size + 1, l):
 			mini_batch.append(self.memory[i])
 
 		for state, action, reward, next_state, done in mini_batch:
